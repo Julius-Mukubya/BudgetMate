b64e06a36ef0e701fd342daf46a6179a1cbf7c41
Julius-Mukubya <mukubyajulius@gmail.com>
2026-06-06 19:15:11 +0300

    feat: integrated firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:budget_mate/firebase_options.dart';
 import 'package:budget_mate/core/theme/app_theme.dart';
 import 'package:budget_mate/core/router/app_router.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: BudgetMateApp(),
